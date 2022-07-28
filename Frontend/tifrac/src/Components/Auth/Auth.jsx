2fdb500345c74364ba8b46c65a0719e8029a5867
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2022-07-28 13:39:25 +0530

    update in file

---
+++ Frontend/tifrac/src/Components/Auth/Auth.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div>
+      <button>Log In</button>
+      <button>Sign Up</button>
+    </div>
+  );
+};
+
+export default Auth;
