08d274b47e4e339e8578c0dcfa52efc5c3c4c910
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2022-07-28 20:58:00 +0530

    updated login

--- Frontend/tifrac/src/App.js
+++ Frontend/tifrac/src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Auth from "./Components/Auth/Auth";
+import Login from "./Components/Auth/Login";
+import Signup from "./Components/Auth/Signup";
+import Home from "./Components/Home/Home";
 function App() {
   return (
     <div className="App">
-      <Auth />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/signup"} element={<Signup />} />
+      </Routes>
     </div>
   );
 }
